464aecdf8106d4d24790fd43318be44e9e919f9d
Ajay Waghray <ajaywaghray@Ajays-MBP.attlocal.net>
2014-09-01 10:03:43 -0500

    Screwing around with creating a model for Incrediband

---
+++ Incrediband/IncredibandModel.swift
@@ -0,0 +1,25 @@
+//
+//  IncredibandModel.swift
+//  Incrediband
+//
+//  Created by Ajay Waghray on 9/1/14.
+//  Copyright (c) 2014 Incrediband, Inc. All rights reserved.
+//
+
+import Foundation
+
+// Here is the class used to hold info when the user is onboarding into Incrediband
+class IncredibandOnboard {
+
+    let instrument: String
+    let location: Double
+    let skillLevel: String
+    
+    // Initializing variables in the onboarding class like a boss.
+    init(instrument:String, location:Double, skillLevel:String) {
+        self.instrument = instrument
+        self.location = location
+        self.skillLevel = skillLevel
+    }
+}
+
