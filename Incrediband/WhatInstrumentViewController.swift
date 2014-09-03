42c4dd06609d264ba06819e478da5db3f0d740f4
Ajay Waghray <ajaywaghray@Ajays-MBP.attlocal.net>
2014-09-02 20:33:01 -0500

    New view controller and Shizz.

---
+++ Incrediband/WhatInstrumentViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WhatInstrumentViewController.swift
+//  Incrediband
+//
+//  Created by Ajay Waghray on 9/2/14.
+//  Copyright (c) 2014 Incrediband, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
