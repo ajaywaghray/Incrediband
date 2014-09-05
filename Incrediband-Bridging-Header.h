d85ed5dd33077f0aa762726f5c3d29848f2086b9
Ajay Waghray <ajaywaghray@Ajays-MBP.attlocal.net>
2014-09-04 23:10:01 -0500

    Added location services stuff and a Swift file for the Where do you play view controller.

--- Incrediband-Bridging-Header.h
+++ Incrediband-Bridging-Header.h
@@ -4,3 +4,4 @@
 
 #import <Parse/Parse.h>
 #import <FacebookSDK/FacebookSDK.h>
+#import <CoreLocation/CoreLocation.h>
---
+++ Incrediband/WherePlayViewController.swift
@@ -0,0 +1,33 @@
+//
+//  WherePlayViewController.swift
+//  Incrediband
+//
+//  Created by Ajay Waghray on 9/4/14.
+//  Copyright (c) 2014 Incrediband, Inc. All rights reserved.
+//
+
+import UIKit
+import foundation
+
+class WherePlayViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func locationSelected(sender: AnyObject){
+        userOnboard.instrument = "guitar"
+    }
+    
+}
