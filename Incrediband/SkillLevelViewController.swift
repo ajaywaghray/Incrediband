68f7f3c51ad6172c07bb12187b97d1e0fd01a5a3
Ajay Waghray <ajaywaghray@Ajays-MBP.attlocal.net>
2014-09-06 15:46:25 -0500

    Hooking up wizard upload to Parse...

---
+++ Incrediband/SkillLevelViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SkillLevelViewController.swift
+//  Incrediband
+//
+//  Created by Ajay Waghray on 9/6/14.
+//  Copyright (c) 2014 Incrediband, Inc. All rights reserved.
+//
+
+import UIKit
+
+class SkillLevelViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func UploadModel(sender: AnyObject){
+        
+    }
+    
+    
+}
