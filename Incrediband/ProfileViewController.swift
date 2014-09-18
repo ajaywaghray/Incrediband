53f7cfc0dbae830f1fac8e647b4fbc6fc462d639
Ajay Waghray <ajaywaghray@Ajays-MBP.attlocal.net>
2014-09-17 20:48:40 -0500

    Added images for the profile view and a view controller for it.

---
+++ Incrediband/ProfileViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewController.swift
+//  Incrediband
+//
+//  Created by Ajay Waghray on 9/17/14.
+//  Copyright (c) 2014 Incrediband, Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
