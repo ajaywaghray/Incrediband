1aa021745c30910655c961019d8c77ca5a404e43
Ajay Waghray <ajaywaghray@Ajays-MBP.home>
2014-10-11 14:25:13 -0500

    Two cells for conversations is sort of working...

--- Incrediband/ConversationViewController.swift
+++ Incrediband/ConversationViewController.swift
@@ -31,27 +31,34 @@ class ConversationViewController: UIKit.UIViewController, UITableViewDataSource,
     
     @IBAction func viewTapped(sender : AnyObject) {
         userConversation.resignFirstResponder()
+        matchConversation.resignFirstResponder()
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
+        var identifier: NSString = "Cell1"
+        //UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
         
-        var cell : UITableViewCell! = tableView .dequeueReusableCellWithIdentifier("conversationCell", forIndexPath: indexPath) as UITableViewCell
+        var cell : UITableViewCell! = tableView .dequeueReusableCellWithIdentifier(identifier, forIndexPath: indexPath) as UITableViewCell
         
         
         
-        if let conversationCell = cell as? ConversationTableViewCell {
-            if let label = conversationCell.userConversation {
-                label.text = "Ajay"
+        if let Cell1 = cell as? UserConversationTableViewCell {
+            if let label = Cell1.userConversation {
+                label.text = "What's up man? Wanna make some music on Tuesday?"
             }
-            if let label = conversationCell.matchConversation {
-                label.text = "Beginner"
+            return Cell1
+        }
+        
+        if let Cell2 = cell as? MatchConversationTableViewCell {
+            if let label = Cell2.matchConversation {
+                label.text = "Hey! Can't make it Tuesday, but how about Sunday?"
             }
-            return conversationCell
+            return Cell2
         }
         
         return cell
@@ -59,9 +66,12 @@ class ConversationViewController: UIKit.UIViewController, UITableViewDataSource,
     
 }
 
-class ConversationTableViewCell: UIKit.UITableViewCell {
+class UserConversationTableViewCell: UIKit.UITableViewCell {
     @IBOutlet var userConversation : UILabel!
+    @IBOutlet var userProfilePicture: UIImageView!
+}
+
+class MatchConversationTableViewCell: UIKit.UITableViewCell {
     @IBOutlet var matchConversation : UILabel!
     @IBOutlet var matchProfilePicture: UIImageView!
-    @IBOutlet var userProfilePicture: UIImageView!
 }
