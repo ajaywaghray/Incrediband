e5051f9f7e4e067ae40553afa8dd3032256ecced
Ajay Waghray <ajaywaghray@Ajays-MBP.home>
2014-10-11 12:21:03 -0500

    Added a table view for the conversation and the view controller for it.

---
+++ Incrediband/ConversationViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ConversationViewController.swift
+//  Incrediband
+//
+//  Created by Ajay Waghray on 10/11/14.
+//  Copyright (c) 2014 Incrediband, Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ConversationViewController: UIKit.UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet var userResultName : UILabel!
+    @IBOutlet var userResultSkillLevel : UILabel!
+    @IBOutlet var userResultProfilePicture: UIImageView!
+    @IBOutlet var userResultInstument: UIImageView!
+    @IBOutlet var usersTable: UITableView!
+    
+    func refreshUI() {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        refreshUI()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func viewTapped(sender : AnyObject) {
+        userResultName.resignFirstResponder()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        
+        var cell : UITableViewCell! = tableView .dequeueReusableCellWithIdentifier("usersCell", forIndexPath: indexPath) as UITableViewCell
+        
+        
+        
+        if let usersCell = cell as? ResultsTableViewCell {
+            if let label = usersCell.userResultName {
+                label.text = "Ajay"
+            }
+            if let label = usersCell.userResultSkillLevel {
+                label.text = "Beginner"
+            }
+            return usersCell
+        }
+        
+        return cell
+    }
+    
+}
+
+class ConversationTableViewCell: UIKit.UITableViewCell {
+    @IBOutlet var userResultName : UILabel!
+    @IBOutlet var userResultSkillLevel : UILabel!
+    @IBOutlet var userResultProfilePicture: UIImageView!
+    @IBOutlet var userResultInstument: UIImageView!
+}
